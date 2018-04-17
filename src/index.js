18d589559838646e871a4cea1d6ccf102c4f8a05
Dmitry Mungalov <d.mungalov@magdv.com>
2018-04-17 15:33:32 +0700

    remove <div> wrap route App

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import React from 'react'
-import { render } from 'react-dom'
+import React        from 'react'
+import { render }   from 'react-dom'
 import { Provider } from 'react-redux'
-import {
-    Route
-  } from 'react-router-dom'
-import App from './containers/App'
-import './app.css'  
+import { Route }    from 'react-router-dom'
+import App          from './containers/App'
+import './app.css'
 import { configureStore, history } from './store/configureStore'
 import { ConnectedRouter } from 'react-router-redux'
 
@@ -14,10 +12,7 @@ export const STORE = configureStore();
 render(
     <Provider store={STORE}>
         <ConnectedRouter history={history}>
-            <div>
-                <Route path="/" component={App} />
-                {/*<Route path="/q=:filter?&sort=:sortType?&direction=:direction?&page=:pageNum?" component={App} />*/}
-            </div>
+            <Route path="/" component={App} />
         </ConnectedRouter>
     </Provider>,
     document.getElementById('root')
